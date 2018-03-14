d802c1bf39b4370edefc438012934fafdc443775
Chris Kelly <cpjkelly@gmail.com>
2018-03-14 11:07:54 -0700

    Add not null constraint to users.username

---
+++ db/migrate/20180314180050_make_user_username_required.rb
@@ -0,0 +1,5 @@
+class MakeUserUsernameRequired < ActiveRecord::Migration[5.1]
+  def change
+      change_column_null(:users, :username, false)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308204957) do
+ActiveRecord::Schema.define(version: 20180314180050) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
