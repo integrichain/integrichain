f2bcaf0b34387a24eb6c207cdd678503f4e896f6
ben-cunningham <bencunningham17@gmail.com>
2018-03-14 17:41:30 -0700

    Add Ability for document permissions

---
+++ app/models/ability.rb
@@ -0,0 +1,9 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.present?
+      docs = Permission.all.where(user_id: user.id)
+      can :manage Document id: docs
+  end
+end
