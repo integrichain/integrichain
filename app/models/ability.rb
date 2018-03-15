c817e157f2bf774d42496014711ee6620aefcf25
ben-cunningham <bencunningham17@gmail.com>
2018-03-14 17:59:04 -0700

    Fixup Ruby

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,6 @@ class Ability
   def initialize(user)
     if user.present?
       docs = Permission.all.where(user_id: user.id)
-      can :manage Document id: docs
+      can :manage, Document, id: docs
   end
 end
