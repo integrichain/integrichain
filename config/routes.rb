5e78a9fd360a17795596b143f00de7ff5df741bd
ben-cunningham <bencunningham17@gmail.com>
2018-03-15 22:28:30 -0700

    Add router for config

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :documents
   resources :users
+  resources :permissions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
