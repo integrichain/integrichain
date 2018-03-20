7500bb97fec439ce224bbfa54235008713530279
ben-cunningham <bencunningham17@gmail.com>
2018-03-20 14:40:21 -0700

    Remove user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,4 @@
 class UsersController < Clearance::UsersController
-  def index
-    @users = User.all
-    render :index
-  end
-
   def show
     @user = User.find(params[:id])
     @documents = @user.documents
