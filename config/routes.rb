46592f3acdc1c81ba1d0a71f4680687c03969ad4
Chris Kelly <cpjkelly@gmail.com>
2018-03-19 20:12:15 -0700

    Move Sign in/out buttons to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :permissions
 
   get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show', as: 'user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
