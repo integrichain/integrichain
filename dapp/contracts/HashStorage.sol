10d88312ce83e1ebcc14fbb5985fb51ee64cf3f8
Chris Kelly <cpjkelly@gmail.com>
2018-03-21 14:22:16 -0700

    Store all hashes in a single contract

--- dapp/contracts/HashStorage.sol
+++ dapp/contracts/HashStorage.sol
@@ -1,9 +1,18 @@
 pragma solidity ^0.4.17;
 
 contract HashStorage {
-  uint256 public hash;
+  uint256[] public hashes;
 
-  function setHash(uint256 newHash) external {
-    hash = newHash;
+  function addHash(uint256 newHash) external returns (uint256){
+    hashes.push(newHash);
+    return hashes.length - 1; // the array index of the new hash
+  }
+
+  function getHashByIndex(uint256 index) external view returns (uint256) {
+    if (index <= hashes.length - 1) { // ensure that the index is not out of bounds
+      return hashes[index];
+    } else {
+      return 0;
+    }
   }
 }
--- dapp/test/TestHashStorage.sol
+++ dapp/test/TestHashStorage.sol
@@ -7,13 +7,22 @@ import "../contracts/HashStorage.sol";
 contract TestHashStorage {
   HashStorage hashStorage = HashStorage(DeployedAddresses.HashStorage());
 
-  function testStoresAndRetrievesHash() public {
+  function testStoresHashWithAddHash() public {
     // some SHA3-224 hash
     uint256 originalHash = uint256(0x4918224b25f43bfa8651156fb47915a3544cc5bbdf59eb72e89bbc36);
 
-    hashStorage.setHash(originalHash);
-    uint256 storedHash = hashStorage.hash();
+    uint256 hashIndex = hashStorage.addHash(originalHash);
 
-    Assert.equal(originalHash, storedHash, "Expected storedHash to equal originalHash");
+    uint256 storedHash = hashStorage.getHashByIndex(hashIndex);
+
+    Assert.equal(storedHash, originalHash, "Retrieved hash should be the same as the original hash that was stored.");
+
+
+    uint256 zeroHash = 0;
+    uint256 outOfBoundsIndex = hashIndex + 1;
+
+    uint256 shouldBeZeroHash = hashStorage.getHashByIndex(outOfBoundsIndex);
+
+    Assert.equal(shouldBeZeroHash, zeroHash, "Retrieved hash should be the same as the original hash that was stored.");
   }
 }
