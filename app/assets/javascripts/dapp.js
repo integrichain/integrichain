7206f12d89b39cc4b9b904b25e591f872dc97651
Chris Kelly <cpjkelly@gmail.com>
2018-03-21 17:11:33 -0700

    Remove unneeded placeholder code

--- app/assets/javascripts/dapp.js
+++ app/assets/javascripts/dapp.js
@@ -3,27 +3,6 @@ App = {
   contracts: {},
 
   init: function() {
-    // Load pets.
-    $.getJSON('/pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
-    return App.initWeb3();
-  },
-
-  initWeb3: function() {
     if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider;
     } else {
@@ -37,18 +16,6 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON('/contracts/Adoption.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
-
-      // Set the provider for our contract
-      App.contracts.Adoption.setProvider(App.web3Provider);
-
-      // Use our  contract to retrieve and mark the adopted pets
-
-      return App.markAdopted();
-    });
     $.getJSON('/contracts/HashStorage.json', function(data) { // initialize the HashStorage contract in JS
       // Get the necessary contract artifact file and instantiate it with truffle-contract
       var HashStorageArtifact = data;
@@ -59,8 +26,6 @@ App = {
 
       return
     });
-
-    return App.bindEvents();
   },
 
   storeHash: function(hash) {
@@ -128,65 +93,8 @@ App = {
         console.log(err.message);
       });
     });
-  },
-
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
-
-  markAdopted: function(adopters, account) {
-    var adoptionInstance;
-
-    App.contracts.Adoption.deployed()
-      .then(function(instance) {
-        adoptionInstance = instance;
-
-        // grab adopters from the adoption contract. call does this without burning gas
-        return adoptionInstance.getAdopters.call();
-      })
-      .then(function (adopters) {
-        console.log(adopters)
-        for (i = 0; i < adopters.length; i++) {
-          if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-            // disable adopt button and set the text to "Success"
-            $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
-          }
-        }
-      })
-      .catch(function (err) {
-        console.log(err.message);
-      });
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    var adoptionInstance;
-
-    web3.eth.getAccounts(function(error, accounts) {
-      if (error) {
-          console.log(error);
-        }
-
-      var account = accounts[0];
-
-      App.contracts.Adoption.deployed().then(function(instance) {
-        adoptionInstance = instance;
-
-        // Execute adopt as a transaction by sending account
-        // A transaction will burn gas
-        return adoptionInstance.adopt(petId, {from: account});
-      }).then(function(result) { // result is a transaction object
-        return App.markAdopted();
-      }).catch(function(err) {
-        console.log(err.message);
-      });
-    });
   }
-
-};
+}
 
 $(function() {
   $(window).on('load', function() {
